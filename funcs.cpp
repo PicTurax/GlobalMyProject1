bc835011178d2abe266c82d002e4496d55e5fd30
Vadim Mihailov <picturax@icloud.com>
2017-12-11 04:51:10 +0300

    add function MyMul()

--- funcs.cpp
+++ funcs.cpp
@@ -1,14 +1,18 @@
 //funcs.cpp
-
 #include "stdafx.h"
 #include "funcs.h"
-
 double MySum(double a, double b)
 {
-	return a + b;
+return a + b;
 }
-
 double MySub(double a, double b)
 {
-	return a - b;
+MySub(double a, double b)
+{
+
+return a - b;
+}
+double MyMul(double a, double b)
+{
+return a * b;
 }
