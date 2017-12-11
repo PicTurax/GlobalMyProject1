29ffefaa9dd967ef4ec903289d2b1754206840e6
Vadim Mihailov <picturax@icloud.com>
2017-12-11 04:40:47 +0300

    Snapshot 3. Add pause in program.cpp

--- program.cpp
+++ program.cpp
@@ -3,12 +3,13 @@
 #include "stdafx.h"
 #include <iostream>
 #include "funcs.h"
-50
 using namespace std;
+
 
 int main()
 {
 	cout << MySum(10, 3) << endl;
 	cout << MySub(10, 3) << endl;
+	system("pause");
 	return 0;
-}
+}
